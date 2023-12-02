a468f425775c5f28343627ce93c0c7d00eaa0583
jungmin.park <pjm9673@gmail.com>
2023-12-02 19:01:21 +0900

    first commit

---
+++ src/main/java/com/example/yetiproject/YetiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.yetiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yetiproject/YetiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yetiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
