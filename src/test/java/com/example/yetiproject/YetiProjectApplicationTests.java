3c3b710d7cd1380b32e7ec93aee6044b7aeebf79
jungmin.park <pjm9673@gmail.com>
2023-12-06 01:08:35 +0900

    test 지움

--- src/test/java/com/example/yetiproject/YetiProjectApplicationTests.java
+++ src/test/java/com/example/yetiproject/YetiProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class YetiProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
