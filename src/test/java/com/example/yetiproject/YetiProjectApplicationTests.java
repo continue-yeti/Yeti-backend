18843a1859f5870844b1872264071b81b955ee20
jungmin.park <pjm9673@gmail.com>
2023-12-06 01:09:54 +0900

    test 지움2

--- src/test/java/com/example/yetiproject/YetiProjectApplicationTests.java
+++ src/test/java/com/example/yetiproject/YetiProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.yetiproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class YetiProjectApplicationTests {
 
 	// @Test
