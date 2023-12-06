ff2278064587fa0c928f0989caf8f035e3f2c4e5
jungmin.park <pjm9673@gmail.com>
2023-12-06 13:32:34 +0900

    Ticket ApiResponse 적용

--- src/main/java/com/example/yetiproject/controller/TicketController.java
+++ src/main/java/com/example/yetiproject/controller/TicketController.java
@@ -1,16 +1,18 @@
 package com.example.yetiproject.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.yetiproject.dto.ApiResponse;
 import com.example.yetiproject.dto.ticket.TicketRequestDto;
+import com.example.yetiproject.dto.ticket.TicketResponseDto;
 import com.example.yetiproject.service.TicketService;
 
 import lombok.RequiredArgsConstructor;
@@ -26,31 +28,31 @@ public class TicketController {
 	// TODO. user jwt 토큰 받기 필요
 	// 예매한 티켓 목록 조회
 	@GetMapping("")
-	public ResponseEntity viewListOfReservedTickets(){
-		return new ResponseEntity(ticketService.getUserTicketList(), HttpStatus.OK);
+	public ApiResponse<List<TicketResponseDto>> viewListOfReservedTickets(){
+		return ApiResponse.success("예매한 티켓 목록 조회에 성공했습니다.", ticketService.getUserTicketList());
 	}
 
 	// TODO. user jwt 토큰 받기 필요
 	// 예매한 티켓 상세 조회
 	@GetMapping("/userId/{userId}/ticketId/{ticketId}")
-	public ResponseEntity detailViewReservedTicket(@PathVariable(name = "userId") Long userId, @PathVariable(name = "ticketId") Long ticketId){
-		System.out.println("[Controller : detailViewReservedTicket userId = ] " + userId);
-		return new ResponseEntity(ticketService.showDetailTicket(userId, ticketId), HttpStatus.OK);
+	public ApiResponse<TicketResponseDto> detailViewReservedTicket(@PathVariable(name = "userId") Long userId, @PathVariable(name = "ticketId") Long ticketId){
+		log.info("[Controller : detailViewReservedTicket userId = ] " + userId);
+		return ApiResponse.success("티켓 상세 조회에 성공했습니다", ticketService.showDetailTicket(userId, ticketId));
 	}
 
 	// TODO. user jwt 토큰 받기 필요
 	// 예매 하기
 	@PostMapping("/reserve/{ticketId}")
-	public ResponseEntity reserveTicket(@PathVariable(name = "ticketId") Long ticketId, @RequestBody TicketRequestDto ticketRequestDto){
+	public ApiResponse<TicketResponseDto> reserveTicket(@PathVariable(name = "ticketId") Long ticketId, @RequestBody TicketRequestDto ticketRequestDto){
 		log.info("TicketController ticket_id = {}", ticketId);
-		return new ResponseEntity(ticketService.reserveTicket(ticketId, ticketRequestDto), HttpStatus.OK);
+		return ApiResponse.success("예매 성공" , ticketService.reserveTicket(ticketId, ticketRequestDto));
 	}
 
 	// TODO. user jwt 토큰 받기 필요
 	// 예매 취소
 	@DeleteMapping("/{ticketId}")
-	public ResponseEntity cancelTicket(@PathVariable(name = "ticketId") Long ticketId){
-		return ticketService.cancelUserTicket(ticketId);
+	public ApiResponse cancelTicket(@PathVariable(name = "ticketId") Long ticketId){
+		return ApiResponse.success("예매 취소 완료" , ticketService.cancelUserTicket(ticketId));
 	}
 
 }
