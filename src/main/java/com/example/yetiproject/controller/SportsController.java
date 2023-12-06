92ee015f834d38ee90b13560f62d7bfdc40bf665
jungmin.park <pjm9673@gmail.com>
2023-12-06 16:28:55 +0900

    Service Test 진행중

--- src/main/java/com/example/yetiproject/controller/SportsController.java
+++ src/main/java/com/example/yetiproject/controller/SportsController.java
@@ -1,18 +1,22 @@
 package com.example.yetiproject.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.yetiproject.dto.ApiResponse;
 import com.example.yetiproject.dto.sports.SportsRequestDto;
 import com.example.yetiproject.dto.sports.SportsResponseDto;
 import com.example.yetiproject.service.SportsService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.nio.charset.StandardCharsets;
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
