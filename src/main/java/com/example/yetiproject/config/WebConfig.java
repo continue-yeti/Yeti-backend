0e7833f92bca05ba4e4b85e99543888a66751f85
jungmin.park <pjm9673@gmail.com>
2023-12-16 00:18:13 +0900

    config 수정

--- src/main/java/com/example/yetiproject/config/WebConfig.java
+++ src/main/java/com/example/yetiproject/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
 
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(url)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "X-Requested-With")
                 .allowCredentials(true);
