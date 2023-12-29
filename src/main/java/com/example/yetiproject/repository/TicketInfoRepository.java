db62cb93ed1bac203d65681639584a1ea9a4903a
jungmin.park <pjm9673@gmail.com>
2023-12-29 15:34:55 +0900

    N+1 ticketInfo 수정

--- src/main/java/com/example/yetiproject/repository/TicketInfoRepository.java
+++ src/main/java/com/example/yetiproject/repository/TicketInfoRepository.java
@@ -1,10 +1,20 @@
 package com.example.yetiproject.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.example.yetiproject.entity.Ticket;
 import com.example.yetiproject.entity.TicketInfo;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TicketInfoRepository extends JpaRepository<TicketInfo, Long> {
+	@Override //기본 적으로 findById 을 제공하기 때문에 Override 하여 재정의 후 사용
+	@EntityGraph(attributePaths = {"sports","sports.stadium"})
+	Optional<TicketInfo> findById(Long ticketId);
 
 }
