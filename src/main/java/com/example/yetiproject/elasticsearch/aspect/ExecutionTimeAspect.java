17f5c474f42486e8e49833b22416ddff01ac7801
Gary9409 <gary9409@gmail.com>
2024-01-07 03:46:44 +0900

    fix : 검색 로직 분리

--- src/main/java/com/example/yetiproject/elasticsearch/aspect/ExecutionTimeAspect.java
+++ src/main/java/com/example/yetiproject/elasticsearch/aspect/ExecutionTimeAspect.java
@@ -5,8 +5,11 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Aspect
 @Component
+@Slf4j(topic = "Search 시간 비교")
 public class ExecutionTimeAspect {
 
 	@Around("execution(* com.example.yetiproject.controller.*.*(..))")
@@ -14,7 +17,7 @@ public class ExecutionTimeAspect {
 		long startTime = System.currentTimeMillis();
 		Object proceed = joinPoint.proceed();
 		long endTime = System.currentTimeMillis();
-		System.out.println(joinPoint.getSignature() + " executed in " + (endTime - startTime) + "ms");
+		log.info(joinPoint.getSignature() + " executed in " + (endTime - startTime) + "ms");
 		return proceed;
 	}
 }
