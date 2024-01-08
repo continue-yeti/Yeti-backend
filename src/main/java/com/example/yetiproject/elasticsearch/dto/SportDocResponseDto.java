09985f67f611c60303199701930677ecef2932d0
hongui <honge7694@naver.com>
2024-01-08 21:25:06 +0900

    SportsResponseDto 수정

--- src/main/java/com/example/yetiproject/elasticsearch/dto/SportDocResponseDto.java
+++ src/main/java/com/example/yetiproject/elasticsearch/dto/SportDocResponseDto.java
@@ -1,21 +1,19 @@
 package com.example.yetiproject.elasticsearch.dto;
 
-import com.example.yetiproject.dto.sports.SportsResponseDto;
 import com.example.yetiproject.elasticsearch.document.SportDoc;
 
-import com.example.yetiproject.entity.Sports;
 import lombok.Data;
 
 @Data
 public class SportDocResponseDto {
 
-	private SportsResponseDto sport;
+	private Long sportId;
 	private String stadiumName;
 	private String matchDate;
 	private String sportName;
 
-	public SportDocResponseDto(SportDoc sportDoc, Sports sports) {
-		this.sport = new SportsResponseDto(sports);
+	public SportDocResponseDto(SportDoc sportDoc) {
+		this.sportId = sportDoc.getSportId();
 		this.stadiumName = sportDoc.getStadiumName();
 		this.matchDate = sportDoc.getMatchDate();
 		this.sportName = sportDoc.getSportName();
