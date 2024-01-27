e66dda66e75488f89c3a18832dd685b3cf2a6aed
jungmin.park <pjm9673@gmail.com>
2024-01-28 00:09:44 +0900

    CD redis 및 slack webhook 구성

--- deploy.sh
+++ deploy.sh
@@ -4,71 +4,36 @@ cd /home/ubuntu
 # 환경변수 DOCKER_APP_NAME 설정
 DOCKER_APP_NAME=ec2-application
 
-# 실행 중인 blue가 있는지 확인
-# 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지를 EXIST_BLUE 변수에 저장
-EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+# 실행 중인 app이 있는지 확인
+# 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지를 EXIST_APP 변수에 저장
+EXIST_APP=$(sudo docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml ps | grep Up)
 
 # 배포 시작한 날짜와 시간을 기록
 echo "배포 시작 일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
 
-# green이 실행 중이면 blue up
-# EXIST_BLUE 변수가 비어있는지 확인
-if [ -z "$EXIST_BLUE" ]; then
+# EXIST_APP 변수가 비어있는지 확인
+if [ -z "$EXIST_APP" ]; then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종류하지 않겠습니다."
+  # 로그 파일(/home/ubuntu/deploy.log)에 "app 배포 : 8080:8080"이라는 내용을 추가
+  echo "app 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
 
-  # 로그 파일(/home/ubuntu/deploy.log)에 "blue up - blue 배포 : port:8081"이라는 내용을 추가
-  echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
-
-	# docker-compose.blue.yml 파일을 사용하여 seniors-blue 프로젝트의 컨테이너를 빌드하고 실행
-	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
-
+	# docker-compose.yml 파일을 사용하여 seniors-blue 프로젝트의 컨테이너를 빌드하고 실행
+	sudo docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml up -d --build
   # 30초 동안 대기
   sleep 30
+  # app 문제없이 배포되었는지 현재 실행 여부를 확인
+  APP_HEALTH=$(sudo docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml ps | grep Up)
 
-  # blue가 문제없이 배포되었는지 현재 실행 여부를 확인
-  BLUE_HEALTH=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
-
-  # blue가 현재 실행 중이지 않다면 -> 런타임 에러 또는 다른 이유로 배포가 되지 못한 상태
-  if [ -z "$BLUE_HEALTH" ]; then
+  # app 현재 실행 중이지 않다면 -> 런타임 에러 또는 다른 이유로 배포가 되지 못한 상태
+  if [ -z "$APP_HEALTH" ]; then
     # slack으로 알람을 보낼 수 있는 스크립트를 실행
-    sudo /home/ubuntu/slack_blue.sh
-  # blue가 현재 실행되고 있는 경우에만 green을 종료
-  else
-
-    # /home/ubuntu/deploy.log: 로그 파일에 "green 중단 시작"이라는 내용을 추가
-    echo "green 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
-
-    # docker-compose.green.yml 파일을 사용하여 seniors-green 프로젝트의 컨테이너를 중지
-    sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
-
-    # 사용하지 않는 이미지 삭제
-    sudo docker image prune -af
-
-    echo "green 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
+    sudo /home/ubuntu/slack_app.sh
   fi
-
-# blue가 실행중이면 green up
 else
-	echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
-	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+  echo "> docker old-ec2-application 종료하겠습니다." >> /home/ubuntu/deploy.log
+  echo "이전버전 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
 
-  sleep 30
-
-  GREEN_HEALTH=$(sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml ps | grep Up)
-
-  if [ -z "$GREEN_HEALTH" ]; then
-
-      sudo ./slack_green.sh
-  else
-
-      # /home/ubuntu/deploy.log: 로그 파일에 "blue 중단 시작"이라는 내용을 추가
-      echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
-
-      # docker-compose.blue.yml 파일을 사용하여 seniors-green 프로젝트의 컨테이너를 중지
-      sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
-
-      # 사용하지 않는 이미지 삭제
-      sudo docker image prune -af
-
-      echo "blue 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
-  fi
+  sudo docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml down
+  sudo docker image prune -af
+  echo "이전버전 삭제 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
 fi
